9855edbc2591c8e16f34b805b69743346b4c40b0
anmiha0672@mail.ru <anmiha0672@mail.ru>
2021-09-01 11:28:41 +0500

    new #5

--- src/main/java/com/wega/wega/controllers/WegaController.java
+++ src/main/java/com/wega/wega/controllers/WegaController.java
@@ -1,16 +1,64 @@
 package com.wega.wega.controllers;
 
+import com.wega.wega.models.Post;
+import com.wega.wega.repo.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WegaController {
 
+    private final PostRepository postRepository;
+
+    public WegaController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
 
     @GetMapping("/blog")
-    public String blogMain(Model model){
+    public String blogMain(Model model) {
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "blog-main";
     }
 
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model) {
+        return "blog-add";
+    }
+
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title,
+                              @RequestParam String anons,
+                              @RequestParam String full_text,
+                              Model model) {
+        Post post = new Post(title, anons, full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/wega/wega/repo/PostRepository.java
@@ -0,0 +1,8 @@
+package com.wega.wega.repo;
+
+import com.wega.wega.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+}
